01fdf95158f605eeffca5171c274b032a07d81f0
David Osborn <davidcosborn@gmail.com>
2018-07-29 22:42:22 +0100

    Add proposal option to plugin for pipeline operator.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = function() {
 			require('@babel/plugin-proposal-nullish-coalescing-operator'),
 			require('@babel/plugin-proposal-numeric-separator'),
 			require('@babel/plugin-proposal-optional-chaining'),
-			require('@babel/plugin-proposal-pipeline-operator'),
+			[require('@babel/plugin-proposal-pipeline-operator'), {proposal: 'minimal'}],
 			require('@babel/plugin-proposal-throw-expressions'),
 			require('@babel/plugin-syntax-dynamic-import'),
 			require('@babel/plugin-syntax-import-meta')
